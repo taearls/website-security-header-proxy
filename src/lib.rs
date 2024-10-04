3257203bf089f8185afca537097bc07229ec6b0e
Tyler Earls <tyler.a.earls@gmail.com>
2024-10-04 18:18:39 -0500

    add security headers

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,28 @@ use worker::*;
 
 #[event(fetch)]
 async fn fetch(
-    _req: HttpRequest,
-    _env: Env,
+    req: Request,
+    env: Env,
     _ctx: Context,
-) -> Result<HttpResponse> {
-    console_error_panic_hook::set_once();
-    Ok(http::Response::builder()
-        .status(http::StatusCode::OK)
-        .body(Body::empty())?)
+) -> Result<Response> {
+    let version = env.var("WORKERS_RS_VERSION")?.to_string();
+    let origin = env.var("ORIGIN")?.to_string();
+    let response = worker::Fetch::Request(req).send().await?;
+
+    let mut headers = response.headers().clone();
+    headers.set(
+        "strict-transport-security",
+        "max-age=31536000; includeSubDomains",
+    )?;
+    headers.set("x-frame-options", "SAMEORIGIN")?;
+    headers.set("x-content-type-options", "nosniff")?;
+    headers.set("referrer-policy", "no-referrer")?;
+    headers.set("permissions-policy", "microphone 'none'")?;
+    headers.set(
+        "content-security-policy",
+        "default-src 'self' 'unsafe-inline' 'unsafe-eval' cloudflareinsights.com *.cloudflareinsights.com",
+    )?;
+    headers.set("access-control-allow-origin", &origin)?;
+    headers.set("cf-worker-version", &version)?;
+    Ok(response.with_headers(headers))
 }
