f1d1fa2545f4a8af3dff734a2c6119f088f33393
Tyler Earls <tyler.a.earls@gmail.com>
2025-04-18 18:54:20 -0500

    add google font to default src

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ async fn fetch(
     headers.set("permissions-policy", "microphone 'none'")?;
     headers.set(
         "content-security-policy",
-        "default-src 'self' cloudflareinsights.com *.cloudflareinsights.com; img-src 'self' *.cloudinary.com",
+        "default-src 'self' cloudflareinsights.com *.cloudflareinsights.com; img-src 'self' *.cloudinary.com; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com",
     )?;
     headers.set("permissions-policy", "microphone=(), geolocation=()")?;
     headers.set("access-control-allow-origin", &origin)?;
